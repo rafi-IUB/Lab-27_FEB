384225463b9308acc986b9c90886a838fd8a58e8
rafi <2222222@iub.edu.bd>
2025-02-27 10:28:11 +0600

    update lable

--- src/main/java/a213/feb27/HelloController.java
+++ src/main/java/a213/feb27/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Maruf Hossain Rafi; 2222222");
     }
 }
